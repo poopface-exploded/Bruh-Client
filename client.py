5c94412d2d69ab0f000b39189fe4a152973698a5
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-02-08 15:42:37 -0800

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.76"
+SERVER = "192.168.1.254"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
